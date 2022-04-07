f995c7ace384e868692fbd8e965d3433cf20a2c3
ezesilva95 <4381@holbertonschool.com>
2022-04-07 10:34:29 -0700

    Simple Shell

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+void execarg(char **args);
+char **stringtok(char *str_line, const char *delimitator);
+char *_which(char **args);
+char *iscommand(char **args);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_getenv(char* str);
+void printenv(void);
+extern char **environ;
+void ctrlC(int sig_num);
+
+#endif
