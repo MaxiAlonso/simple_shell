267be2b0f42eee40b9557b60407becb16da80026
MaxiHBTN <4387@holbertonschool.com>
2022-04-08 01:53:59 +0000

    Simple Shell

--- ctrlC.c
+++ ctrlC.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * ctrlC - function to catch the ctrl + c signal
+ * @sig_num: the signal sent when we press Ctrl+C
+ **/
+
 void ctrlC(int sig_num)
 {
 	if (sig_num == SIGINT)
