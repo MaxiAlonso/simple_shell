fa96abe5f4e4f060875781e6be4d0d7d2633c0cf
ezesilva95 <4381@holbertonschool.com>
2022-04-07 19:09:31 -0700

    Simple Shell

--- main.h
+++ main.h
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/types.>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <signal.h>
 
 void execarg(char **args);
 char **stringtok(char *str_line, const char *delimitator);
