6692303a21a2e628b5158e0888ace37386263102
MaxiHBTN <4387@holbertonschool.com>
2022-04-09 22:14:36 +0000

    Simple Shell

--- execargs.c
+++ execargs.c
@@ -12,40 +12,36 @@ void execarg(char **args)
 	int status, succes = 0;
 	struct stat st;
 
-	if (args[0] != NULL)
+	if (args[0][0] >= 'a' && args[0][0] <= 'z')
 	{
-		if (args[0][0] >= 'a' && args[0][0] <= 'z')
+		args[0] = iscommand(args);
+		if (args[0] == NULL)
 		{
-			args[0] = iscommand(args);
-			if (args[0] == NULL)
-			{
-				free(args[0]);
-				return;
-			}
-			succes = 1;
+			free(args[0]);
+			return;
 		}
-		if (stat(args[0], &st) == 0)
+		succes = 1;
+	}
+	if (stat(args[0], &st) == 0)
+	{
+		child_pid = fork();
+		if (child_pid == 0)
 		{
-			child_pid = fork();
-			if (child_pid == 0)
+			if (execve(args[0], args, environ) == -1)
 			{
-				if (execve(args[0], args, environ) == -1)
-				{
-					perror(args[0]);
-					free(args[0]);
-					free(args);
-					exit(-1);
-				}
+				perror(args[0]);
+				free(args);
+				exit(-1);
 			}
-			else
-				wait(&status);
-			if (succes == 1)
-				free(args[0]);
 		}
 		else
-		{
-			write(STDOUT_FILENO, args[0], _strlen(args[0]));
-			write(STDOUT_FILENO, ": not found\n", 12);
-		}
+			wait(&status);
+		if (succes == 1)
+			free(args[0]);
+	}
+	else
+	{
+		write(STDOUT_FILENO, args[0], _strlen(args[0]));
+		write(STDOUT_FILENO, ": not found\n", 12);
 	}
 }
